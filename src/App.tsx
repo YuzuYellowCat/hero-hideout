0055bc3b0ef439ce8a8c750869577110bfa422f6
YuzuCat <sponge_solder02@icloud.com>
2025-04-04 22:30:20 -0700

    prettier tweaks and hex effect works below content now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css';
-import ColorfulHex from './components/ColorfulHex';
+import "./App.css";
+import ColorfulHex from "./components/ColorfulHex";
 
 function App() {
-  return (
-    <div className="bg">
-      <ColorfulHex />
-    </div>
-  );
+    return (
+        <div className="site">
+            <div className="contents">
+                <div className="section">
+                    <h1>YuzuCat</h1>
+                    <h3>beats up TurboWolf</h3>
+                </div>
+            </div>
+            <div className="bg">
+                <ColorfulHex />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ColorfulHex/index.tsx
+++ src/components/ColorfulHex/index.tsx
@@ -1,5 +1,11 @@
-import React, { useCallback, useEffect, createRef, useState, useMemo } from "react"
-import "./index.css"
+import React, {
+    useCallback,
+    useEffect,
+    createRef,
+    useState,
+    useMemo,
+} from "react";
+import "./index.css";
 
 type FadePoint = {
     x: number;
@@ -8,12 +14,12 @@ type FadePoint = {
     duration: number;
     frame: number;
     speed: number;
-}
+};
 
 type CanvasSize = {
     width: number;
     height: number;
-}
+};
 
 const ColorfulHex: React.FC = () => {
     const canvasRef = createRef<HTMLCanvasElement>();
@@ -21,82 +27,106 @@ const ColorfulHex: React.FC = () => {
     const [context, setContext] = useState<CanvasRenderingContext2D>();
     const [canvasSize, setCanvasSize] = useState<CanvasSize>({
         width: window.innerWidth,
-        height: window.innerHeight
-    })
+        height: window.innerHeight,
+    });
 
     const draw = useCallback(() => {
-        if (!context) { return; }
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+        if (!context) {
+            return;
+        }
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
         for (const point of points) {
             const opacity = 1 - Math.min(point.frame / point.duration, 1);
             context.fillStyle = point.getColor(opacity);
-            context.beginPath()
-            context.arc(point.x, point.y, point.frame * point.speed, 0, Math.PI * 2, false);
-            context.fill()
+            context.beginPath();
+            context.arc(
+                point.x,
+                point.y,
+                point.frame * point.speed,
+                0,
+                Math.PI * 2,
+                false
+            );
+            context.fill();
             point.frame++;
         }
-      }, [context, points]);
+    }, [context, points]);
+
+    const onMouseMove = useCallback(
+        (event: MouseEvent) => {
+            points.push({
+                x: event.clientX,
+                y: event.clientY,
+                frame: 0,
+                getColor: (opacity) => `rgba(0, 120, 120, ${opacity})`,
+                duration: 50,
+                speed: 1,
+            });
+        },
+        [points]
+    );
+
+    const onClick = useCallback(
+        (event: MouseEvent) => {
+            points.push({
+                x: event.clientX,
+                y: event.clientY,
+                frame: 0,
+                getColor: (opacity) => `rgba(0, 255, 255, ${opacity})`,
+                duration: 70,
+                speed: 2,
+            });
+        },
+        [points]
+    );
 
     useEffect(() => {
-        const potentialContext = canvasRef.current?.getContext('2d');
+        const potentialContext = canvasRef.current?.getContext("2d");
         if (potentialContext) {
-            setContext(potentialContext)
+            setContext(potentialContext);
         }
-    }, [canvasRef])
+    }, [canvasRef]);
 
     useEffect(() => {
         let animationFrameId: number;
-    
+
         const render = () => {
-            draw()
-            animationFrameId = window.requestAnimationFrame(render)
-        }
-        render()
-        
+            draw();
+            animationFrameId = window.requestAnimationFrame(render);
+        };
+        render();
+
         return () => {
-            window.cancelAnimationFrame(animationFrameId)
-        }
+            window.cancelAnimationFrame(animationFrameId);
+        };
     }, [draw]);
 
     useEffect(() => {
         const resizeListener = () => {
-            setCanvasSize({ width: window.innerWidth, height: window.innerHeight })
-        }
-        window.addEventListener('resize', resizeListener);
+            setCanvasSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+
+        window.addEventListener("resize", resizeListener);
+        window.addEventListener("click", onClick);
+        window.addEventListener("mousemove", onMouseMove);
         return () => {
-            window.removeEventListener('resize', resizeListener);
-        }
-    }, [])
-
-    const onMouseMove = useCallback((event: React.MouseEvent<HTMLCanvasElement>) => {
-        points.push(
-            {
-                x: event.clientX,
-                y: event.clientY,
-                frame: 0,
-                getColor: (opacity) => `rgba(120, 120, 0, ${opacity})`,
-                duration: 50,
-                speed: 1,
-            }
-        )
-    }, [points]);
-
-    const onClick = useCallback((event: React.MouseEvent<HTMLCanvasElement>) => {
-        points.push(
-            {
-                x: event.clientX,
-                y: event.clientY,
-                frame: 0,
-                getColor: (opacity) => `rgba(255, 255, 0, ${opacity})`,
-                duration: 70,
-                speed: 2,
-            }
-        )
-    }, [points])
+            window.removeEventListener("resize", resizeListener);
+            window.removeEventListener("click", onClick);
+            window.removeEventListener("mousemove", onMouseMove);
+        };
+    }, [onMouseMove, onClick]);
 
     return (
-        <canvas className="hex" ref={canvasRef} width={canvasSize.width} height={canvasSize.height} onMouseMove={onMouseMove} onClick={onClick} />
-    )
-}
+        <canvas
+            className="hex"
+            ref={canvasRef}
+            width={canvasSize.width}
+            height={canvasSize.height}
+        />
+    );
+};
 
 export default ColorfulHex;

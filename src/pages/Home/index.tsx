ac2f591f578dd1f894985d247e4c780ab9563f59
YuzuCat <sponge_solder02@icloud.com>
2025-04-10 20:26:13 -0700

    test gh pages deploy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,26 +4,9 @@ import PageWrapper from "components/PageWrapper";
 const Home: React.FC = () => {
     return (
         <PageWrapper color="#1b86a7" title="Home">
-            Portal 2 is a 2011 puzzle-platform game developed by Valve for
-            Windows, macOS, Linux, PlayStation 3, and Xbox 360. The digital PC
-            versions are distributed online by Valve's Steam service, while all
-            retail editions are distributed by Electronic Arts. A port for the
-            Nintendo Switch was released as part of the Portal: Companion
-            Collection in June 2022.
-            <br />
-            <br />
-            Like the original Portal (2007), players solve puzzles by placing
-            portals and teleporting between them. Portal 2 adds features
-            including tractor beams, lasers, light bridges, and paint-like gels
-            that alter player movement or allow portals to be placed on any
-            surface. In the single-player campaign, players control Chell, who
-            navigates the dilapidated Aperture Science Enrichment Center during
-            its reconstruction by the supercomputer GLaDOS (Ellen McLain); new
-            characters include robot Wheatley (Stephen Merchant) and Aperture
-            founder Cave Johnson (J. K. Simmons). In the new cooperative mode,
-            players solve puzzles together as robots Atlas and P-Body (both
-            voiced by Dee Bradley Baker). Jonathan Coulton and the National
-            produced songs for the game.
+            This is a WIP website for YuzuCat! This will eventually be an
+            organized place for suiting pics, refs, and more! But for now, it's
+            just an empty cardboard box. Which is fun too.
         </PageWrapper>
     );
 };
